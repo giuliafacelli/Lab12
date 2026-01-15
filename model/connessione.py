c0711daba10512da7dd6af9870f696bff9c0f876
giuliafacelli <giulia.facelli05@gmail.com>
2026-01-15 14:32:47 +0100

    soluzione

---
+++ model/connessione.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from model.rifugio import Rifugio
+import datetime
+
+@dataclass
+class Connessione:
+    rifugio1: Rifugio
+    rifugio2: Rifugio
+    distanza: float
+    difficolta: str
+    durata: datetime.time
+    anno: int
+
+    def __str__(self):
+        return (f'Sentiero: {self.rifugio1} - {self.rifugio2} --> {self.distanza} km'
+                f'Difficolta: {self.difficolta}'
+                f'Durata: {self.durata}'
+                f'Anno: {self.anno}')
+
+    def __repr__(self):
+        return (f'Sentiero: {self.rifugio1} - {self.rifugio2} --> {self.distanza} km'
+                f'Difficolta: {self.difficolta}'
+                f'Durata: {self.durata}'
+                f'Anno: {self.anno}')
--- model/rifugio.py
+++ model/rifugio.py
@@ -5,12 +5,15 @@ class Rifugio:
     id: int
     nome: str
     localita: str
+    altitudine: int
+    capienza: int
+    aperto: bool
 
     def __str__(self):
         return f'[{self.id}] {self.nome} ({self.localita})'
 
-    def __eq__(self, other):
-        return isinstance(other, Rifugio) and self.id == other.id
+    def __repr__(self):
+        return f'[{self.id}] {self.nome} ({self.localita})'
 
     def __hash__(self):
         return hash(self.id)
